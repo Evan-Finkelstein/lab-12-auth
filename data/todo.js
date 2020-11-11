3a4a3466776e5fba0db7ffe26926bf3eba7e4a3c
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-11-10 16:28:32 -0800

    initial

---
+++ data/todo.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'wash the dishes',
+    completed: false,
+  },
+  {
+    id: 2,
+    todo: 'sweep',
+    completed: false,
+  },
+  {
+    id: 3,
+    todo: 'laundry',
+    completed: false,
+  },
+];
