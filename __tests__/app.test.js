6402a839bba92e7687825bc46134b1f3b5decece
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-11-10 17:31:43 -0800

    mvp

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,53 +9,125 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token; // eslint-disable-line
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns todo', async () => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+          'id': 4,
+          'todo': 'bessie',
+          'completed': false,
+          'owner_id': 2
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+          'id': 5,
+          'todo': 'jumpy',
+          'completed': false,
+          'owner_id': 2
         },
         {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+          'id': 6,
+          'todo': 'spot',
+          'completed': false,
+          'owner_id': 2
         }
       ];
 
+      await fakeRequest(app)
+        .post('/api/todo')
+        .send(expectation[0])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      await fakeRequest(app)
+        .post('/api/todo')
+        .send(expectation[1])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      await fakeRequest(app)
+        .post('/api/todo')
+        .send(expectation[2])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/api/todo')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+    test('returns todo', async () => {
+
+      const expectation = [
+        {
+          'id': 7,
+          'todo': 'bessie',
+          'completed': false,
+          'owner_id': 2
+        },
+
+      ];
+
+      const data = await fakeRequest(app)
+        .post('/api/todo')
+        .send(expectation[0])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+    test('returns todo', async () => {
+
+      const expectation = [
+        {
+          'id': 7,
+          'todo': 'bessie',
+          'completed': true,
+          'owner_id': 2
+        },
+      ]
+      const input = [
+        {
+          'id': 7,
+          'todo': 'bessie',
+          'completed': false,
+          'owner_id': 2
+        },
+
+      ];
+
+      const data = await fakeRequest(app)
+        .put('/api/todo/7')
+        .send(input[0])
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
         .expect(200);
 
